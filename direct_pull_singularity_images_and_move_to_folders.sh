9c0784f5fb00ca5bfa6d925118d1d59fcd7efc9a
root <root@troytestbox2.us-central1-a.c.bioinfo-devel.internal>
2024-03-25 14:52:21 +0000

    update to make sure augustus is pulled in singularity

--- direct_pull_singularity_images_and_move_to_folders.sh
+++ direct_pull_singularity_images_and_move_to_folders.sh
@@ -26,3 +26,5 @@ singularity pull flag_transdecoder.image docker://ghcr.io/formbio/flag_transdeco
 mv flag_transdecoder.image containers/transdecoder/
 singularity pull flag_trinity.image docker://ghcr.io/formbio/flag_trinity:latest
 mv flag_trinity.image containers/trinity/
+singularity pull flag_augustus.image docker://ghcr.io/formbio/flag_augustus:latest
+mv flag_augustus.image containers/augustus/
